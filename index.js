b57c5237087d2ee5c5a19c625b198f2009502b1d
vebradev <justinas@vebra.dev>
2019-11-17 15:33:09 +0200

    move server config to server.js

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-
-const server = express();
+const server = require('./server');
 const port = process.env.PORT;
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
-
 server.get('/', (req, res) => {
-  res.send("Up!");
+  res.send('🌚');
 });
 
 server.listen(port, () => {
