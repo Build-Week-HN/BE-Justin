44f8f9e3633a6098fc28b7ea9c37096d001073a1
vebradev <justinas@vebra.dev>
2019-11-18 17:03:12 +0200

    cleanup stories route

--- routes/stories.js
+++ routes/stories.js
@@ -1,17 +1,15 @@
-const router = require('express').Router();
-const STORIES = require('../models/stories');
+const router = require("express").Router();
+const STORIES = require("../models/stories");
 
-router.get('/', (req, res) => {
-  STORIES
-    .find()
-    .then(stories => {
-      res.status(200).json(stories);
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: err.message
-      })
-    })
+router.get("/", async (req, res) => {
+  try {
+    const stories = await STORIES.returnAll();
+    res.status(200).json(stories);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
 });
 
 module.exports = router;
