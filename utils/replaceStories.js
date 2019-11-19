6de248c402eb045d0b8acc37c982d39e053a400c
vebradev <justinas@vebra.dev>
2019-11-19 17:13:42 +0200

    refactor code to check if data is fetched first

--- utils/replaceStories.js
+++ utils/replaceStories.js
@@ -3,33 +3,35 @@ const STORIES = require("../models/stories");
 
 const replaceStories = async () => {
   try {
-    const API =
-      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty";
-    const fetchedStories = await axios.get(API);
-    const top20stories = fetchedStories.data;
-    top20stories.length = 20;
+    const API = "https://hacker-news.firebaseio.com/v0/topstories.json";
+    const { data: top20stories } = await axios.get(API);
+    
+    if (top20stories.length > 0) {
+      await STORIES.cleanUp();
+      top20stories.length = 20;
 
-    await STORIES.cleanUp();
-
-    top20stories.map(async storyID => {
-      try {
-        let API = `https://hacker-news.firebaseio.com/v0/item/${storyID}.json`;
-        const story = await axios.get(API);
-        const { id, by, descendants, score, time, title, url } = story.data;
-
-        await STORIES.add({
-          id,
-          author: by,
-          comments_count: descendants,
-          score,
-          time,
-          title,
-          url
-        });
-      } catch (err) {
-        console.log(err.message);
-      }
-    });
+      top20stories.map(async storyID => {
+        try {
+          let API = `https://hacker-news.firebaseio.com/v0/item/${storyID}.json`;
+          const story = await axios.get(API);
+          const { id, by, descendants, score, time, title, url } = story.data;
+  
+          await STORIES.add({
+            id,
+            author: by,
+            comments_count: descendants,
+            score,
+            time,
+            title,
+            url
+          });
+        } catch (err) {
+          console.log(err.message);
+        }
+      });
+    } else {
+      console.log(`Error: top20stories length is ${top20stories.length}.`);
+    }
   } catch (err) {
     console.log(err.message);
   }
