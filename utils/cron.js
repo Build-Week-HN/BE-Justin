01f7f46bce00df6af56fed13274ded29154365eb
vebradev <justinas@vebra.dev>
2019-11-19 17:13:08 +0200

    setup cron job every 5min

--- utils/cron.js
+++ utils/cron.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const replaceStories = require('./replaceStories');
 
-cron.schedule('* * * * * ', () => {
-  console.log('db cleaned & populated');
+cron.schedule('*/5 * * * *', () => {
+  console.log('5 minutes passed - db cleaned & populated.');
   replaceStories();
 })
