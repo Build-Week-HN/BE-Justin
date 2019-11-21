23e5bcedb208d0a02cb74d4bdf0cc5c42ff53d29
vebradev <justinas@vebra.dev>
2019-11-21 13:49:31 +0200

    add production key for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/dev.sqlite3'
+      filename: "./data/dev.sqlite3"
     },
     useNullAsDefault: true,
     pool: {
@@ -21,36 +20,14 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
-
 };
